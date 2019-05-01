9e726894c8f707bec92017f3404aab7dc9efc915
Avlant Nilsson <avlant@hotmail.com>
2019-05-01 16:24:51 +0200

    shift to data instead of mean and std

--- src/loadExpdata.m
+++ src/loadExpdata.m
@@ -1,23 +1,31 @@
 function [expData, removalPoints, growth] = loadExpdata(folder, organism, condition)
-keySet =   {'Sulfate', 'Riboflavin', 'Folate', 'Pantothenate', 'Niacinamide', 'Choline', 'Phosphate', 'Albumin', 'AGPC', 'Urea', 'Amonia', 'Lactate', 'Glucose', 'Pyruvate', 'Pyroglutamate', 'Alanine', 'Arginine', 'Asparagine', 'Aspartic acid', 'Cystine', 'Glutamic acid', 'Glutamine', 'Glycine', 'Histidine', 'Iso-leucine', 'Leucine', 'Lysine', 'Methionine', 'Ornithine', 'Phenylalanine', 'Serine', 'Taurine', 'Threonine', 'Tryptophan', 'Tyrosine', 'Valine'};
-valueSet = {'sulfate[s]', 'riboflavin[s]', 'folate[s]', 'pantothenate[s]', 'nicotinamide[s]', 'choline[s]', 'Pi[s]', 'albumin[s]', 'sn-glycerol-3-PC[s]', 'urea[s]', 'NH3[s]', 'L-lactate[s]', 'glucose[s]', 'pyruvate[s]', '5-oxoproline[s]', 'alanine[s]', 'arginine[s]', 'asparagine[s]', 'aspartate[s]', 'cystine[s]', 'glutamate[s]', 'glutamine[s]', 'glycine[s]', 'histidine[s]', 'isoleucine[s]', 'leucine[s]', 'lysine[s]', 'methionine[s]', 'ornithine[s]', 'phenylalanine[s]', 'serine[s]', 'taurine[s]', 'threonine[s]', 'tryptophan[s]', 'tyrosine[s]', 'valine[s]'};
-metTranslation = containers.Map(keySet,valueSet);
+metaboliteMap = IO([folder '/metaboliteMap.tsv']);
+metTranslation = containers.Map(metaboliteMap(:,1),metaboliteMap(:,2));
 
-pathing = [folder '/' organism '-' condition '-'];
+pathing = [folder '/' organism '-serialized.txt'];
 expData = containers.Map;
 
 %Met data
+metData = IO(pathing);
+colNames = metData(1,:);
+conditionCol = ismember(colNames, 'condition');
+metCol = ismember(colNames, 'met');
+timeCol = ismember(colNames, 'time');
+valueCol = ismember(colNames, 'value');
+timeAndValue = or(timeCol, valueCol);
 
-metData = importdata([pathing 'mets.txt']);
-
-for i = 2:length(metData.textdata)
-    currentMet = metData.textdata{i,1};  
-    currentMet = metTranslation(currentMet);
-    currentData = metData.data(i-1,:)';
-    if not(expData.isKey(currentMet))
-        expData(currentMet) = currentData;
-    else
-        expData(currentMet) = [expData(currentMet) currentData];
+for i = 2:size(metData, 1)
+    curCondtion = metData{i,conditionCol};
+    if strcmp(condition, curCondtion)
+        currentMet = metData{i, metCol};
+        currentMet = metTranslation(currentMet);
+        curVal = cell2nummat(metData(i,timeAndValue))';
+        curVal = [curVal; 0]; %for a standard deviation slot
+        if not(expData.isKey(currentMet))
+            expData(currentMet) = curVal;
+        else
+            expData(currentMet) = [expData(currentMet) curVal];
+        end
     end
 end
 
@@ -31,15 +39,24 @@ if addStdValues
         curData(3,:) = stdData(i);
         expData(stdMets{i}) = curData;
     end
+end
 
 %Growth data
-growthData = importdata([folder '/growthData.txt']);
-growthLabels = growthData.textdata(:,1);
-growthLabels(1) = []; %Remove header
-growthRows = ismember(growthLabels, [organism '-' condition '-']);
-growth = growthData.data(growthRows,:);
+pathing = [folder '/' organism '-growthData.txt'];
+growthData = IO(pathing);
+conditionCol = ismember(colNames, 'condition');
+growthData = growthData(2:end,:);
+
+timeCol = ismember(colNames, 'time');
+valueCol = ismember(colNames, 'value');
+timeAndValue = or(timeCol, valueCol);
+
+matchingCondition = ismember(growthData(:,conditionCol), condition);
 
 
+growth = cell2nummat(growthData(matchingCondition, timeAndValue));
+
+%Sampling points
 removalPoints = importdata([folder '/' organism '-' 'volume.txt']); %sample removal points
 end
 
