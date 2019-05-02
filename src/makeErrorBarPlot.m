39bf46e397c790a68a9b3cd88bae3a66b4006766
Avlant Nilsson <avlant@hotmail.com>
2019-05-02 11:25:42 +0200

    Shift to lb and ub instead of error

--- src/makeErrorBarPlot.m
+++ src/makeErrorBarPlot.m
@@ -1,28 +1,22 @@
-function makeErrorBarPlot(data, error, labels, color)
+function xvals = makeErrorBarPlot(data, errorLB, errorUB, labels, color)
 hold all
 totalWidth = 0.5;
 errorWidth = 1.5*totalWidth/size(data,2);
 % backgroundColor = [0.95 0.95 0.95
 %                    0.9  0.9  0.9];
-minVal = min(min(data)) - max(max(error));
-maxVal = max(max(data)) + max(max(error));
+minVal = min(min([data errorLB errorUB]));
+maxVal = max(max([data errorLB errorUB]));
 
-
-for i = 1:size(data,1)
-%     bgColor = mod(i,2) + 1;
-%     x = i + 0.5*[-1 1 1 -1];
-%     y = [minVal minVal maxVal maxVal];
-%     fill(x,y,backgroundColor(bgColor,:), 'LineStyle', 'none', 'HandleVisibility','off') 
-    
+for i = 1:size(data,1)    
     xvals = i + linspace(-totalWidth/2,totalWidth/2,size(data,2));
+    
     for j = 1:size(data,2)
         x = xvals(j);
         y = data(i,j);
-        e = error(i,j);
+        eLB = y-errorLB(i,j);
+        eUB = y-errorUB(i,j);
         bar(x, y, errorWidth, 'LineStyle','none', 'FaceColor', color(j,:))
-        if e>0
-            errorbar(x, y, e, 'k', 'LineStyle','none', 'HandleVisibility','off');
-        end
+        errorbar(x, y, eLB, eUB, 'k', 'LineStyle','none', 'HandleVisibility','off');
     end
 end
 xticks(1:size(data,1))
