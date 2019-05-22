c00a507b8fb78c70bdb9e3baed0e1e2ceca73ee6
Avlant Nilsson <avlant@hotmail.com>
2019-05-22 19:00:18 +0200

    hardcode path to graphviz

--- displayNetwork/makePdf.py
+++ displayNetwork/makePdf.py
@@ -1,9 +1,13 @@
 import os
+import sys
 from graphviz import Source
-os.chdir('D:\stories\Glutamine\matlab\displayNetwork')
-file = open('test.dot', 'r')#READING DOT FILE
+os.environ["PATH"] += os.pathsep + 'C:/Program Files (x86)/Graphviz2.38/bin/'
+
+path = sys.argv[1]
+fileEnding = sys.argv[2]
+file = open(path, 'r')#READING DOT FILE
 text=file.read()
-s = Source(text, filename='test.dot', format="pdf")
+s = Source(text, filename=path, format=fileEnding)
 
 s.view()
 file.close()
