cb5858b9c7c9306a921adf7f4e72b38094b48564
Avlant Nilsson <avlant@hotmail.com>
2019-05-22 18:48:19 +0200

    suppress output

--- src/estimateExponential.m
+++ src/estimateExponential.m
@@ -7,7 +7,7 @@ yvals = zeros(N,1);
         Y1 = data(i,2);
         Y2 = data(i+1,2);        
         dt = X2-X1;
-        mu = log(Y2/Y1)/dt
+        mu = log(Y2/Y1)/dt;
         xinterval = and(X1<=xvals, xvals<=X2);
         curX = xvals(xinterval)-X1;
         curY = Y1.*exp(mu.*curX);
