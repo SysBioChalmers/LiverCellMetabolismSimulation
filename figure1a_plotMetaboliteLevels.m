b440d941d57e7194b22c41a186e557d65938cc26
Avlant Nilsson <avlant@hotmail.com>
2019-05-22 19:08:08 +0200

    display vitamine concentrations

--- figure1a_plotMetaboliteLevels.m
+++ figure1a_plotMetaboliteLevels.m
@@ -4,15 +4,14 @@ addpath('src')
 global massPerCell
 massPerCell = 1000 * 426.8 * 10^-12; %mg per cell
 
+showVitamins = true;
 
-model = setupBiomass(model, 150, 0.5);
+model = setupBiomass(model, 48, 1);
 
-
-
-celltype = 'hepg2';
-condition = '6mm';
+celltype = 'hepg2'; %hepg2 or huh7
+condition = '22'; %0, 6 or 22 mM glucose
 [expData, volumePoints, growthdat] = loadExpdata('data', celltype, condition);
-[fluxMets, fluxValues] = loadFluxes('fluxvalues', [celltype '-' condition '-.txt']);
+[fluxMets, fluxValues] = loadFluxes('fluxvalues', celltype, condition);
 
 outputMets = ['biomass[s]';expData.keys'];
 
@@ -23,34 +22,50 @@ tspan = [startT 100];
 init = getInitialConditions(expData,growthdat,startT);
 
 haltCrit = zeros(length(outputMets),1);
-
-
-haltCrit(findIndex(outputMets, 'glutamine[s]')) = 1200;
-
-%prevent zero metabolites from triggering
+if strcmp(celltype, 'hepg2')
+    haltCrit(findIndex(outputMets, 'glutamine[s]')) = 1200;
+else
+    haltCrit(findIndex(outputMets, 'glutamine[s]')) = 1400;
+end
+    
+%prevent zero metabolites from triggering growth phase shift
 haltCrit(findIndex(outputMets, 'urea[s]')) = -1000;
 haltCrit(findIndex(outputMets, '5-oxoproline[s]')) = -1000;
 haltCrit(findIndex(outputMets, 'NH3[s]')) = -1000;
 haltCrit(findIndex(outputMets, 'asparagine[s]')) = -1000; 
 haltCrit(findIndex(outputMets, 'aspartate[s]')) = -1000;
-%haltCrit(findIndex(outputMets, 'choline[s]')) = -1000;
-
+haltCrit(findIndex(outputMets, 'choline[s]')) = -1000;
+haltCrit(findIndex(outputMets, 'pantothenate[s]')) = -1000;
 
 newFluxValues = fluxValues;
 %newFluxValues = fitMetabolicFluxes(model, tspan, fluxMets, newFluxValues, halflife, outputMets, init, haltCrit, volumePoints, expData);
 
 [t, y, yconc, breakPoints, mu] = fullSimulation(model, tspan, fluxMets, newFluxValues, halflife, outputMets, init, haltCrit, volumePoints, true);
 
-
+%%
 close all
 dict = expData.keys;
-dict = {'glucose[s]', 'L-lactate[s]', 'pyruvate[s]', 'glutamine[s]', 'glutamate[s]', 'alanine[s]', 'arginine[s]', 'asparagine[s]', 'aspartate[s]', 'cystine[s]',  'glycine[s]', 'histidine[s]', 'isoleucine[s]', 'leucine[s]', 'lysine[s]', 'methionine[s]', 'phenylalanine[s]', 'serine[s]',  'threonine[s]', 'tyrosine[s]', 'valine[s]'};
-%dict = {'glucose[s]', 'L-lactate[s]', 'pyruvate[s]', 'glutamine[s]', 'glutamate[s]', 'alanine[s]'};
-%dict = { 'arginine[s]', 'asparagine[s]', 'aspartate[s]', 'taurine[s]', 'cystine[s]',  'glycine[s]', 'histidine[s]', 'isoleucine[s]', 'leucine[s]', 'lysine[s]', 'methionine[s]', 'phenylalanine[s]', 'serine[s]',  'threonine[s]', 'tyrosine[s]', 'valine[s]'};
-%dict = {'choline[s]', 'pantothenate[s]', 'folate[s]', 'nicotinamide[s]',  'riboflavin[s]'};
-%dict = {'taurine[s]'};
-dict = {'glucose[s]', 'L-lactate[s]', 'glutamine[s]', 'pyruvate[s]', 'glutamate[s]', 'alanine[s]'};
+%dict = {'glucose[s]', 'L-lactate[s]', 'pyruvate[s]', 'glutamine[s]', 'glutamate[s]', 'alanine[s]', 'arginine[s]', 'asparagine[s]', 'aspartate[s]', 'cystine[s]',  'glycine[s]', 'histidine[s]', 'isoleucine[s]', 'leucine[s]', 'lysine[s]', 'methionine[s]', 'phenylalanine[s]', 'serine[s]',  'threonine[s]', 'tyrosine[s]', 'valine[s]'};
+dict = {'glucose[s]', 'L-lactate[s]', 'glutamine[s]', 'glutamate[s]', 'alanine[s]', 'phenylalanine[s]'};
+%dict = {'glucose[s]', 'L-lactate[s]', 'glutamine[s]', 'alanine[s]'};
 
-plotResults(t, y, yconc, breakPoints, mu, expData, growthdat, dict)
+numberOfCols = 3;
+plotResults(t, y, yconc, breakPoints, mu, expData, growthdat, dict, numberOfCols)
+
+addpath('additionalSamples')
+addpath('additionalSamples/src')
+%overlayData(dict, condition, numberOfCols)
 
 %getGrowthLimitingMet(model, fluxMets, fluxValues(:,1)/1000)
+
+%%
+
+
+if showVitamins
+    figure()
+    dict = {'choline[s]', 'pantothenate[s]', 'folate[s]', 'nicotinamide[s]',  'riboflavin[s]', '5-oxoproline[s]'};
+    dict = {'choline[s]', 'pantothenate[s]', 'nicotinamide[s]', '5-oxoproline[s]'};
+
+    numberOfCols = length(dict);
+    plotResults(t, y, yconc, breakPoints, mu, expData, growthdat, dict, numberOfCols)
+end
