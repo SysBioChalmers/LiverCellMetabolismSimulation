5bd4dfa0fdab07e132c8d816affedced09304fe9
Avlant <avlant@hotmail.com>
2019-09-06 09:44:55 -0400

    modified GAM and NGAM to match rest of project

--- displayBiomass.m
+++ displayBiomass.m
@@ -1,7 +1,7 @@
 clc
 load('model/genericHuman2')
 addpath('src')
-model = setupBiomass(model, 150, 0.5);
+model = setupBiomass(model, 48, 1);
 
 fprintf('Metabolite\tConcentration\tMol weight\tg/gdw\n');
 displayReactionComponents(model, 'HumanGrowth')
